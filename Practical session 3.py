aadb6429cd80d4394e37c6c709a98b0959eb8bae
Yaseen4 <32266012+Yaseen4@users.noreply.github.com>
2017-10-02 14:32:01 +0100

    Add files via upload

---
+++ Practical session 3.py
@@ -0,0 +1,8 @@
+print("Hello, this is a calculating system")
+number1 = int(input("please enter a number"))
+print("thank you")
+number2 = int(input("please enter another number"))
+print("thanks again")
+print(number1 + number2)
+
+
