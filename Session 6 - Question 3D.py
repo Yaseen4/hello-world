e7ae868f2770c348973d3f88a5390c2b7aa4686d
Yaseen4 <32266012+Yaseen4@users.noreply.github.com>
2017-12-04 23:38:09 +0000

    Add files via upload

---
+++ Session 6 - Question 3D.py
@@ -0,0 +1,13 @@
+letterGrade= int(input("Enter your mark: "))
+if letterGrade >= 90 :
+    print("A")
+elif letterGrade >=80 :
+    print("B")
+elif letterGrade >=70 :
+    print("C")
+elif letterGrade >=60 :
+    print("D")
+else:
+    print("F")
+
+    
